879c9b0c7c90acfac4f5ff3b548d2283928d2a64
MelihUluhan <melihuluhann@gmail.com>
2024-05-14 19:04:12 +0300

    EditGarden page updated.

--- pages/EditGarden.jsx
+++ pages/EditGarden.jsx
@@ -6,6 +6,7 @@ import {
     ScrollView,
     Image,
 } from 'react-native';
+
 import { ToastAndroid } from 'react-native'; // Add this line
 import LinearGradient from 'react-native-linear-gradient';
 import React, { useState } from 'react';
@@ -13,6 +14,8 @@ import styles from '../styles/Style';
 import { useRoute } from '@react-navigation/native';
 import { updateGarden } from '../services/garden_services';
 import { useTranslation } from 'react-i18next';
+import PhotoPick from '../layouts/photo_picker/ImagePicker';
+import storage from '@react-native-firebase/storage';
 
 const EditGarden = ({ navigation }) => {
     const { t } = useTranslation();
@@ -23,17 +26,50 @@ const EditGarden = ({ navigation }) => {
     
     const [gardenNote, setTextInputValue] = useState('');
     const [gardenName, setGardenName] = useState(garden.name);
+    const [image, setSelectedImage] = useState(null);
+    const [isImageCleared, setIsImageCleared] = useState(false);
+
+    const onSelectImage = (image) => {
+        if (!image) {
+          setSelectedImage(null);
+        } else {
+          setSelectedImage(image);
+          setIsImageCleared(false);
+        }
+      };
+
+    const uploadImage = async (imageUri, folderName) => {
+        const imageRef = storage().ref(`${folderName}/${imageUri.split('/').pop()}`);
+        const response = await fetch(imageUri);
+        const blob = await response.blob();
+    
+        await imageRef.put(blob);
+      };
+    
+      const getImageUrl = async (folderName, imageName) => {
+        const imageRef = storage().ref(`${folderName}/${imageName}`);
+        return await imageRef.getDownloadURL();
+      };
 
     const handleUpdateGarden = async () => {
         const updated_garden = garden
+        let imageUrl = null;
+        if (image != null && image.path != null) {
+          const imageName = image.path.split('/').pop();
+          await uploadImage(image.path, 'gardens');
+          imageUrl = await getImageUrl('gardens', imageName);
+        
+        }
         if(newGardenArea.length > 2){
             updated_garden.polygon = newGardenArea
         }
         if(gardenName.length > 0){
             updated_garden.name = gardenName
         }
+          updated_garden.image_url = imageUrl;
         try {
             await updateGarden(garden.id, updated_garden)
+        
             ToastAndroid.show(t("toast1_eg"), ToastAndroid.SHORT)
             navigation.navigate("Gardens")
             // TODO: Navigate?
@@ -43,16 +79,23 @@ const EditGarden = ({ navigation }) => {
     };
     return (
         <LinearGradient colors={['#89C6A7', '#89C6A7']} style={{ height: '100%' }}>
-            <View
+            <View  
                 style={{
                     padding: 20,
                     flex: 1,
-                    marginBottom: 150,
                 }}>
                 <ScrollView>
                     <View style={{ marginBottom: 90 }}>
+                        
                         <Text style={styles.subtext}> &gt; {garden.name}</Text>
                         <Text style={styles.text}>{t("edit_garden")}</Text>
+                        <Text style={styles.t4}>{t("edit_garden_photo")}</Text>
+                        <PhotoPick
+            onSelect={onSelectImage}
+            isCleared={isImageCleared}
+            setIsCleared={setIsImageCleared}
+            
+          />
                         <Text style={styles.t4}>{t("edit_garden_name")}</Text>
                         <TextInput
                             value={gardenName}
@@ -86,6 +129,8 @@ const EditGarden = ({ navigation }) => {
                             </TouchableOpacity>
                             
                         </View>
+       
+                        
                         <TouchableOpacity style={styles.button_right} onPress={handleUpdateGarden}>
                             <Text style={styles.bt1}> {t("update")} </Text>
                         </TouchableOpacity>
